2a14cf443e5160ad148e48e8250197b956211b78
Ryan Bourhil <bourhilr@gmail.com>
2018-03-22 13:42:55 +0000

    add security to API with various HTTP headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const app = express()
 
 const mongoose = require('mongoose')
@@ -11,12 +12,20 @@ mongoose.connect(config.url, (err) => {
     err ? console.log('ERROR connecting to db: ' + err) : console.log('Succeeded connected to db')
 })
 
+app.use(helmet())
 app.use(bodyParser.json())
 
 /* Allow Cross Origin Resource Sharing */
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET")
+        return res.status(200).json({})
+    }
     next()
 })
 
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = require('./app')
-
 const port = 8000
 
 const server = app.listen(process.env.PORT || 8080, () => {
